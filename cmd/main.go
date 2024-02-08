f0d534d9c383ad8208d5de7ee9738d215de9b12d
Jeovah Simões <jeovahfialho@gmail.com>
2024-02-08 12:27:50 -0300

    DRY solved.

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
-	"strings"
 	"time"
 
 	"payment-platform-solid/internal/store"
 	"payment-platform-solid/pkg/api"
+	"payment-platform-solid/pkg/auth"
 	"payment-platform-solid/pkg/bank"
 	"payment-platform-solid/pkg/middleware"
 	"payment-platform-solid/pkg/payment"
@@ -37,38 +37,6 @@ func (s *AuthService) GenerateToken(userID string) (string, error) {
 	return token.SignedString([]byte(s.SecretKey))
 }
 
-// JWTMiddleware to protect routes
-func JWTMiddleware(next http.Handler, secretKey string) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		tokenString := r.Header.Get("Authorization")
-		_, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			return []byte(secretKey), nil
-		})
-
-		if err != nil || !strings.HasPrefix(tokenString, "Bearer ") {
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-// LoginHandler to authenticate users and issue JWT tokens
-func LoginHandler(authService *AuthService) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		userID := "testuser" // Simulated user ID
-		token, err := authService.GenerateToken(userID)
-		if err != nil {
-			http.Error(w, "Failed to generate token", http.StatusInternalServerError)
-			return
-		}
-
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(token))
-	}
-}
-
 // ProtectedHandler is an example of a protected route
 func ProtectedHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
@@ -94,7 +62,7 @@ func main() {
 	paymentHandler := api.PaymentHandler{Service: paymentService}
 
 	// Configure all routes, applying JWT middleware to protected endpoints
-	http.HandleFunc("/login", LoginHandler(authService))
+	http.HandleFunc("/login", auth.LoginHandler((*auth.AuthService)(authService)))
 	http.Handle("/protected-route", jwtMiddleware.Handler(http.HandlerFunc(ProtectedHandler)))
 	http.Handle("/process-payment", jwtMiddleware.Handler(http.HandlerFunc(paymentHandler.ProcessPayment)))
 	http.Handle("/query-payment", jwtMiddleware.Handler(http.HandlerFunc(paymentHandler.QueryPaymentDetails)))
